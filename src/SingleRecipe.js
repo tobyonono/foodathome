fc0bcc10ec02a50a1ed2a0ecf3ee4ab53f5138c2
tobyonono <ononop@tcd.ie>
2022-02-24 19:56:43 +0000

    removed description

--- src/SingleRecipe.js
+++ src/SingleRecipe.js
@@ -11,7 +11,7 @@ const SingleRecipe = ({ id, title, image, recipeDescription, author }) => {
                 <div className='recipeInfo'>
                     <h3 className="category">Food</h3>
                     <h1 className="recipeTitle">{title}</h1>
-                    <p className="recipeDescription">{recipeDescription}</p>
+                    <p className="recipeDescription"></p>
                     <div className="bottom">
                         <h5 className="author">{author} • 23-12-99</h5>
                         <a href="#" className="getMore"><h5 >Full Recipe → </h5></a>
