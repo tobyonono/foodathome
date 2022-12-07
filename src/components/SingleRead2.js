c38b129412e37df4192216184f8b8714faffc3ce
tobyonono <ononop@tcd.ie>
2022-12-07 19:04:06 +0000

    vercel deploy test

--- src/components/SingleRead2.js
+++ src/components/SingleRead2.js
@@ -2,37 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-const SingleRead = ({ id, name, author, image, description, twitter, instagram }) => {
-    console.log(image);
+const SingleRead = ({ id, name, author, image, description, twitter, instagram }) => (
 
-    return (
-        <li className="relative grid grid-cols-[repeat(12,_1fr)] items-center font-nitti mb-20 p-3 border font-nitti text-sm uppercase shadow-lg rounded-lg">
-            <div className=" relative col-[7_/_-1] row-[1_/_1] text-right">
-                <div className=''>
-                    <p className="m-2 text-xs text-white ">{author}</p>
-                    <h3 className="mb-6 text-[bisque] text-2xl relative z-10">{name}</h3>
-                    <div className="relative z-10 p-5 bg-white shadow-lg border-8 border-solid border-[bisque]">
-                        <p className=" text-black text-left font-spaceGrotesk normal-case">{description}</p>
-                    </div>
-                    <ul className="relative flex flex-wrap z-10 mt-6 mb-3 justify-end text-white">
-                        <a href={twitter}><li className="border border-white p-1 m-1 shadow-lg hover:bg-white hover:text-black">Twitter</li> </a>
-                        <a href={instagram}><li className="border border-white p-1 m-1 shadow-lg hover:bg-white hover:text-black">Instagram</li> </a>
-
-                    </ul>
+    <li className="relative grid grid-cols-[repeat(12,_1fr)] items-center font-nitti mb-20 p-3 border font-nitti text-sm uppercase shadow-lg rounded-lg">
+        <div className=" relative col-[7_/_-1] row-[1_/_1] text-right">
+            <div className=''>
+                <p className="m-2 text-xs text-white ">{author}</p>
+                <h3 className="mb-6 text-[bisque] text-2xl relative z-10">{name}</h3>
+                <div className="relative z-10 p-5 bg-white shadow-lg border-8 border-solid border-[bisque]">
+                    <p className=" text-black text-left font-spaceGrotesk normal-case">{description}</p>
                 </div>
+                <ul className="relative flex flex-wrap z-10 mt-6 mb-3 justify-end text-white">
+                    <a href={twitter}><li className="border border-white p-1 m-1 shadow-lg hover:bg-white hover:text-black">Twitter</li> </a>
+                    <a href={instagram}><li className="border border-white p-1 m-1 shadow-lg hover:bg-white hover:text-black">Instagram</li></a>
+                </ul>
             </div>
-            <div className="relative col-[1_/_8] row-[1_/_-1]">
-                <a className='' href='http://www.rachelama.com'>
-                    <div className='hover:brightness-50 cursor-pointer'><img src={image} /></div>
-                </a>
-            </div>
-        </li>
+        </div>
+        <div className="relative col-[1_/_8] row-[1_/_-1]">
+            <a className='' href='http://www.rachelama.com'>
+                <div className='hover:brightness-50 cursor-pointer'><img src={image} /></div>
+            </a>
+        </div>
+    </li>
 
-
-
-
-    )
-
-}
+)
 
 export default SingleRead;
--- src/components/SingleRecipe2.js
+++ src/components/SingleRecipe2.js
@@ -3,14 +3,12 @@ import '../Recipe.css';
 import { useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
 
-const SingleRecipe = ({ id, title, image, recipeDescription, author}) => {
+const SingleRecipe = ({ id, title, image, recipeDescription, author }) => (
 
-
-    return (
-        <article className="w-full p-3 border font-nitti text-sm uppercase shadow-lg rounded-lg bg-[bisque] hover:bg-[#ffe4c4d3]" key={id}>
+    <article className="w-full p-3 border font-nitti text-sm uppercase shadow-lg rounded-lg bg-[bisque] hover:bg-[#ffe4c4d3]" key={id}>
         <div className="relative p-2 pb-4 border border-[bisque] bg-black h-full">
             <div className="hover:brightness-50 cursor-pointer">
-                <img className ='aspect-square object-cover'src={image} />
+                <img className='aspect-square object-cover' src={image} />
             </div>
             <div className="flex flex-wrap justify-between my-3">
                 <span>{author}</span>
@@ -19,16 +17,16 @@ const SingleRecipe = ({ id, title, image, recipeDescription, author}) => {
                     <h1 className='text-[bisque]'>{title}</h1>
                 </div>
                 <div className="flex flex-row py-4">
-                    <p className="text-left normal-case line-clamp-3 font-spaceGrotesk">{recipeDescription ? recipeDescription :'Click on "Full Recipe" to try a delicious recipe from our Tasty curators. No description from the API so you have this twaddle instead.'}</p>
+                    <p className="text-left normal-case line-clamp-3 font-spaceGrotesk">{recipeDescription ? recipeDescription : 'Click on "Full Recipe" to try a delicious recipe from our Tasty curators. No description from the API so you have this twaddle instead.'}</p>
                 </div>
                 <Link to={`recipes/${title}/${id}`} >
-                            <h5 className="text-xs">Full Recipe → </h5>
-                        </Link>
+                    <h5 className="text-xs">Full Recipe → </h5>
+                </Link>
             </div>
         </div>
     </article>
-    )
-}
+)
+
 
 /* <Link to={`recipes/${title}/${id}`} >
                             <h5 className="getMore">Full Recipe → </h5>
